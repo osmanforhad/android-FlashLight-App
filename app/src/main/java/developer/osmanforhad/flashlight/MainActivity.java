d54a604c3db1aa5a7672cf76a0d6ca57d2edf404
Osman Forhad <osmanforhad93@gmail.com>
2022-04-16 19:39:06 +0600

    project setup for android flashlight app

---
+++ app/src/main/java/developer/osmanforhad/flashlight/MainActivity.java
@@ -0,0 +1,14 @@
+package developer.osmanforhad.flashlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
